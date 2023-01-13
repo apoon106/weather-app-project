57ba4e31ee67b810ce768b0e7bd5aa0ca0cc8d16
apoon106 <121922430+apoon106@users.noreply.github.com>
2023-01-12 23:49:29 -0500

    Showing the current weather

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "56c5825ct0o4b59ab43d3ba391f24bff";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query={New York}&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
